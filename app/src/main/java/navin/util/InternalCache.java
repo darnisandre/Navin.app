18394dbe714aaf9fb60072adcb90979fcaa1e4fa
guilherme <guilherme.armage@acad.pucrs.br>
2016-10-15 23:26:37 -0300

    Adicionando método para salvar rotas personalizadas no cache

--- app/src/main/java/navin/util/InternalCache.java
+++ app/src/main/java/navin/util/InternalCache.java
@@ -5,6 +5,7 @@ import android.location.Location;
 
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +24,7 @@ public class InternalCache {
     private static final String CATEGORIES_KEY = "categories_%s";
     private static final String ROUTE_KEY = "route_%s";
     private static final String BEACON_MAPPING_KEY = "beacon_mapping_%s";
+    private static final String ROUTES_PERSONALIZED_KEY = "routes_personalized_%s";
 
     private final RestClient restClient;
     private final Context context;
@@ -35,26 +37,40 @@ public class InternalCache {
     public List<RouteDTO> getRoutes(final int locationId) {
         String routesKey = String.format(ROUTES_KEY, locationId);
         List<RouteDTO> routes = (List<RouteDTO>) InternalStorage.readObject(context, routesKey);
-        if(routes == null){
+        if (routes == null) {
             routes = restClient.getRoutes(locationId);
             InternalStorage.writeObject(context, routesKey, routes);
         }
         return routes;
     }
 
+    public List<RouteDTO> getRoutesPersonalized(final int locationId) {
+        String routesKey = String.format(ROUTES_PERSONALIZED_KEY, locationId);
+        List<RouteDTO> routes = (List<RouteDTO>) InternalStorage.readObject(context, routesKey);
+        if (routes == null) {
+            return new ArrayList<RouteDTO>();
+        }
+        return routes;
+    }
+
+    public void setRoutesPersonalized(final int locationId, final List<RouteDTO> routes) {
+        String routesKey = String.format(ROUTES_PERSONALIZED_KEY, locationId);
+        InternalStorage.writeObject(context, routesKey, routes);
+    }
+
     public void refreshRoutes(final int locationId) {
         String routesKey = String.format(ROUTES_KEY, locationId);
         List<RouteDTO> routes = null;
-        if(routes == null){
+        if (routes == null) {
             routes = restClient.getRoutes(locationId);
             InternalStorage.writeObject(context, routesKey, routes);
         }
     }
 
-    public List<CategoryDTO> getCategories(final int locationId){
+    public List<CategoryDTO> getCategories(final int locationId) {
         String routeKey = String.format(CATEGORIES_KEY, locationId);
         List<CategoryDTO> categories = (List<CategoryDTO>) InternalStorage.readObject(context, routeKey);
-        if(categories == null){
+        if (categories == null) {
             categories = restClient.getCategories(locationId);
             InternalStorage.writeObject(context, routeKey, categories);
         }
@@ -64,7 +80,7 @@ public class InternalCache {
     public RouteDTO getRoute(final int id) {
         String routeKey = String.format(ROUTE_KEY, id);
         RouteDTO route = (RouteDTO) InternalStorage.readObject(context, routeKey);
-        if(route == null){
+        if (route == null) {
             route = restClient.getRoute(id);
             InternalStorage.writeObject(context, routeKey, route);
         }
@@ -74,7 +90,7 @@ public class InternalCache {
     public BeaconMappingDTO getBeaconMapping(final int locationId) {
         String beaconMappingKey = String.format(BEACON_MAPPING_KEY, locationId);
         BeaconMappingDTO mapping = (BeaconMappingDTO) InternalStorage.readObject(context, beaconMappingKey);
-        if(mapping == null){
+        if (mapping == null) {
             mapping = restClient.getBeaconMapping(locationId);
             InternalStorage.writeObject(context, beaconMappingKey, mapping);
         }
@@ -82,39 +98,40 @@ public class InternalCache {
     }
 
     public List<LocationDTO> getLocations(final double lat, double longitude) {
-        List<LocationDTO> locations= restClient.getLocations(lat, longitude);
-        if(locations!=null){
-            HashMap<Long,LocationDTO> cacheLocations = getCacheLocations();
-            for(LocationDTO l : locations){
+        List<LocationDTO> locations = restClient.getLocations(lat, longitude);
+        if (locations != null) {
+            HashMap<Long, LocationDTO> cacheLocations = getCacheLocations();
+            for (LocationDTO l : locations) {
                 LocationDTO cacheLocation = cacheLocations.get(l.getId());
-                if(cacheLocation != null && l.getLastUpdated().after(cacheLocation.getLastUpdated())){
+                if (cacheLocation != null && l.getLastUpdated().after(cacheLocation.getLastUpdated())) {
                     cleanCacheLocation(l.getId());
                 }
-                cacheLocations.put(l.getId(),l);
+                cacheLocations.put(l.getId(), l);
             }
             storeCacheLocations(cacheLocations);
         }
         return locations;
     }
+
     public LocationDTO getLocation(final Long id) {
-        HashMap<Long,LocationDTO> cacheLocations = getCacheLocations();
-        if(cacheLocations.get(id)!=null){
+        HashMap<Long, LocationDTO> cacheLocations = getCacheLocations();
+        if (cacheLocations.get(id) != null) {
             return cacheLocations.get(id);
         }
-        LocationDTO location= restClient.getLocation(id);
+        LocationDTO location = restClient.getLocation(id);
         return location;
     }
 
     public List<LocationDTO> getLocations() {
-        List<LocationDTO> locations= restClient.getLocations();
-        if(locations!=null){
-            HashMap<Long,LocationDTO> cacheLocations = getCacheLocations();
-            for(LocationDTO l : locations){
+        List<LocationDTO> locations = restClient.getLocations();
+        if (locations != null) {
+            HashMap<Long, LocationDTO> cacheLocations = getCacheLocations();
+            for (LocationDTO l : locations) {
                 LocationDTO cacheLocation = cacheLocations.get(l.getId());
-                if(cacheLocation != null && l.getLastUpdated().after(cacheLocation.getLastUpdated())){
+                if (cacheLocation != null && l.getLastUpdated().after(cacheLocation.getLastUpdated())) {
                     cleanCacheLocation(l.getId());
                 }
-                cacheLocations.put(l.getId(),l);
+                cacheLocations.put(l.getId(), l);
             }
             storeCacheLocations(cacheLocations);
         }
@@ -122,14 +139,15 @@ public class InternalCache {
     }
 
     private void cleanCacheLocation(Long locationId) {
-        InternalStorage.deleteObject(context,String.format(BEACON_MAPPING_KEY, locationId));
+        InternalStorage.deleteObject(context, String.format(BEACON_MAPPING_KEY, locationId));
         List<RouteDTO> routes = (List<RouteDTO>) InternalStorage.readObject(context, String.format(ROUTES_KEY, locationId));
-        if(routes != null){
-            for(RouteDTO route : routes){
+        if (routes != null) {
+            for (RouteDTO route : routes) {
                 InternalStorage.deleteObject(context, String.format(ROUTE_KEY, route.getId()));
             }
         }
         InternalStorage.deleteObject(context, String.format(ROUTES_KEY, locationId));
+        InternalStorage.deleteObject(context, String.format(ROUTES_PERSONALIZED_KEY, locationId));
         InternalStorage.deleteObject(context, String.format(CATEGORIES_KEY, locationId));
     }
 
@@ -137,9 +155,9 @@ public class InternalCache {
         InternalStorage.writeObject(context, LOCATION_KEY, cacheLocations);
     }
 
-    private HashMap<Long, LocationDTO> getCacheLocations(){
-        HashMap<Long, LocationDTO>  map = (HashMap<Long, LocationDTO>) InternalStorage.readObject(context, LOCATION_KEY);
-        if(map==null){
+    private HashMap<Long, LocationDTO> getCacheLocations() {
+        HashMap<Long, LocationDTO> map = (HashMap<Long, LocationDTO>) InternalStorage.readObject(context, LOCATION_KEY);
+        if (map == null) {
             map = new HashMap<Long, LocationDTO>();
         }
         return map;
